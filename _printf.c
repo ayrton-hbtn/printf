e8ba0e7264e21f9d75192f696fd384b03dfc9e74
hippocampus3282 <3282@holbertonschool.com>
2021-07-24 15:36:42 -0700

    Project 0x11 C - printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdarg.h>
-
 /**
  * _printf - prints text to screen, redirects num of bytes to stdout
  * @format: character string
@@ -8,43 +7,41 @@
  */
 int _printf(const char *format, ...)
 {
-	/* va_list vl; */
+	va_list vl;
 	int i = 0, count = 0, flag = 0;
 
 	if (format)
 	{
+		va_start(vl, format);
 		for (; format[i] != '\0'; i++)
 		{
 			if (!flag)
 			{
 				if (format[i] != '%')
-				{
 					count += _putchar(format[i]);
-				}
 				else
-				{
 					flag = 1;
-				}
 			}
 			else
 			{
 				switch (format[i])
 				{
 				case 'c':
-					_putchar('h');
+					count += _putchar(va_arg(vl, int));
 					break;
-
 				case 's':
-					_putchar('t');
+					count += print_str(va_arg(vl, char *));
 					break;
-
 				case '%':
-					_putchar('.');
+					count += _putchar('%');
 					break;
+				case '\n':
+					_putchar('\n');
 				}
 				flag = 0;
 			}
 		}
+		va_end(vl);
 	}
 	return (count);
 }
--- p_str.c
+++ p_str.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- */
+  * print_str - print a string of char
+  * @c: pointer to string
+  *
+  */
+int print_str(char *c)
+{
+	int i = 0;
+
+	if (c)
+	{
+		for (; c[i] != '\0'; i++)
+		{
+			_putchar(c[i]);
+		}
+	}
+	return (i);
+}
