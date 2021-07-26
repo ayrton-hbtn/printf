b182682c6ca9247d51a87f0ef7561916d52a83a4
hippocampus3282 <3282@holbertonschool.com>
2021-07-25 18:01:00 -0700

    Project 0x11 C - printf

---
+++ p_Str.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+  * p_Str - prints string with custom conversion specifier
+  * @c: pointer to string
+  * Description - special and non printable characters are replaced by their
+  * ASCII code in hex
+  * Return: int, strlen
+  */
+int print_Str(char *c)
+{
+	int i; 
+
+	for (i = 0; c[i]; i++)
+	{
+		if ((c[i] >= 0 && c[i] <= 32) || (c[i] >= 127))
+		{
+			_putchar('\\');
+			_putchar('x');
+			if (c[i] <= 16)
+				_putchar('0');
+			print_hex(c[i]);
+		}
+		else
+		{
+			_putchar(c[i]);
+		}
+	}
+	return (i);
+}
