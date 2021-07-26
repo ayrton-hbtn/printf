94bb480b60233f3ed81ef3ec641b794fec950379
hippocampus3282 <3282@holbertonschool.com>
2021-07-26 10:03:58 -0700

    Project 0x11 C - printf

--- p_int.c
+++ p_int.c
@@ -2,28 +2,29 @@
 
 /**
  * print_int - print integers
- * @i: int
+ * @num: int
  *
- * Return: int
+ * Return: int, byte count
  */
 int print_int(int num)
 {
 	int x = num;
 	int i = 0;
 
-	if (num == 0)
-		_putchar('0');
 	if (num)
 	{
 		if (num < 0)
 		{
-			_putchar('-');
+			i += _putchar('-');
 			x = -x;
 		}
 		if ((x / 10) > 0)
 			print_int(x / 10);
 		i += _putchar((x % 10) + '0');
 	}
+	else
+	{
+		i += _putchar(num + '0');
+	}
 	return (i);
 }
-
